94d5d6482cad4062463afd8b78af2d12773372a6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-25 14:43:21 +0000

    Deploy CBHPingPong/cbhpingpong.github.io to github.com/CBHPingPong/cbhpingpong.github.io.git:gh-pages

---
+++ src/whoweare/index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+  <!-- Trang web được lập trình bởi Dương Tùng Anh - C4K60 Chuyên Hà Nam -->
+<!-- Mọi thông tin chi tiết xin liên hệ https://facebook.com/tunnaduong/ -->
+<html>
+	<head>
+    	<link href="/style.css" rel="stylesheet">
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		  
+<meta name="description" content="Cổng thông tin điện tử CLB Bóng bàn THPT Chuyên Hà Nam">
+  <meta name="author" content="">
+
+		<!-- Bộ mã Bootstrap 4 -->
+    	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- Bộ mã jQuery -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
+		<!-- Bộ mã Popper JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+		<!-- Bộ mã JavaScript cho Bootstrap 4 -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+		<!-- Bộ mã riêng cho giao diện trang web -->
+    	<link href="https://v4-alpha.getbootstrap.com/examples/narrow-jumbotron/narrow-jumbotron.css" rel="stylesheet">
+    	<link rel="stylesheet" href="style.css">
+    	<!-- Bộ mã Font Awesome -->
+    	<script src="https://kit.fontawesome.com/5468db3c8c.js" crossorigin="anonymous"></script>
+
+		<title>CPC - CBH Ping Pong Club</title>
+		<style type="text/css">
+
+		#bg {
+			position:fixed; 
+			top:0%; 
+			left:0%; 
+			width:100%; 
+			height:100%;
+		}
+		#bg img {
+			position:absolute; 
+			top:0; 
+			left:0; 
+			right:0; 
+			bottom:0; 
+			margin:auto; 
+			min-width:50%;
+			min-height:50%;
+		}
+		
+		#page-wrap { position: relative; z-index: 2; width: 400px; margin: 50px auto; padding: 20px; background: white; -moz-box-shadow: 0 0 20px black; -webkit-box-shadow: 0 0 20px black; box-shadow: 0 0 20px black; }
+	@media screen and (max-width: 768px) {
+	#page-wrap {
+  zoom: 0.7;
+}
+}
+		</style>
+	</head>
+	<body>
+		<center>
+
+<div id="page-wrap" style="
+    width: 500px;
+">
+		<a href="/"><img src="/logo.png" width="200"></a>
+		<h1>CPC - CBH Ping Pong Club</h1>
+		<h3>Về chúng mình</h3>
+		<p>Chào mừng các bạn đã đến với trang chủ của CLB Bóng bàn THPT Chuyên Biên Hoà - Hà Nam. Nơi đây là chỗ để chúng mình tuyển thành viên cũng như đăng lên những sự kiện, thông tin của CLB chúng mình.
+
+		</p>
+		<hr>
+		<h3>Các thành viên sáng lập:</h3>
+		<p>Dương Tùng Anh - 12 Nga (Chủ tịch CLB)<br>
+			Phạm Thành - 11 Lý (Phó Chủ tịch)
+		</p>
+		
+		<a href="https://facebook.com/CBHPingPongClub"><i class="fab fa-facebook"></i>     </a><a href="https://instagram.com/CBHPingPong"><i class="fab fa-instagram"></i>     </a> <a href="https://github.com/CBHPingPong/"><i class="fab fa-github"></i></a>
+		<h6><i class="fas fa-code" aria-hidden="true"></i> with <i class="fas fa-heart" aria-hidden="true"></i> by <a href="https://fb.me/tunnaduong">Dương Tùng Anh</a></h6>
+		</div>
+		<div id="bg">
+			<img src="/bg.jpg" id="bg" alt="">
+		</div>
+		<script type="text/javascript">
+			function WhoWeAre() {
+				location.href = "/WhoWeAre";
+			}
+			function SignUp() {
+				location.href = "/SignUp";
+			}
+			function Terms() {
+				location.href = "/terms";
+			}
+			function News() {
+				location.href = "/News";
+			}
+			function Photos() {
+				location.href = "/photos";
+			}
+		</script>
+		</center>
+	</body>
